5d0392f255e0327c32e4b495f039aa626746df7f
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-03-01 16:00:56 +0100

    fix: fixed the search for delegations and undelegations

--- cmd/export/exporter.go
+++ cmd/export/exporter.go
@@ -6,6 +6,8 @@ import (
 	"sort"
 	"time"
 
+	coretypes "github.com/tendermint/tendermint/rpc/core/types"
+
 	"github.com/cosmos/cosmos-sdk/simapp/params"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/vesting/exported"
@@ -69,7 +71,6 @@ func (e *Exporter) getLimitHeight() (int64, error) {
 // FixVestingAccount fixes the given account by properly tracking all delegations and
 // undelegations that it has ever performed
 func (e *Exporter) FixVestingAccount(account exported.VestingAccount) error {
-	messageQuery := "message.action = '%[1]s' AND message.sender = '%[2]s' AND tx.height <= %[3]d"
 	accountAddress := account.GetAddress().String()
 
 	height, err := e.getLimitHeight()
@@ -77,22 +78,35 @@ func (e *Exporter) FixVestingAccount(account exported.VestingAccount) error {
 		return err
 	}
 
+	var txs []*coretypes.ResultTx
+
 	// Get the delegation txs
-	delegateQuery := fmt.Sprintf(messageQuery, sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}), accountAddress, height)
-	delegateTxs, err := types.QueryTxs(e.node, delegateQuery)
+	delegateTxs, err := e.getTransactions("delegate", accountAddress, height)
 	if err != nil {
 		return err
 	}
+	txs = append(txs, delegateTxs...)
+
+	delegateTxs, err = e.getTransactions(sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}), accountAddress, height)
+	if err != nil {
+		return err
+	}
+	txs = append(txs, delegateTxs...)
 
 	// Get the unbonding txs
-	undelegateQuery := fmt.Sprintf(messageQuery, sdk.MsgTypeURL(&stakingtypes.MsgUndelegate{}), accountAddress, height)
-	undelegateTxs, err := types.QueryTxs(e.node, undelegateQuery)
+	undelegateTxs, err := e.getTransactions("begin_unbonding", accountAddress, height)
 	if err != nil {
 		return err
 	}
+	txs = append(txs, undelegateTxs...)
 
-	// Merge the transactions and order them by ascending height
-	txs := append(delegateTxs, undelegateTxs...)
+	undelegateTxs, err = e.getTransactions(sdk.MsgTypeURL(&stakingtypes.MsgUndelegate{}), accountAddress, height)
+	if err != nil {
+		return err
+	}
+	txs = append(txs, undelegateTxs...)
+
+	// Order the txs by ascending height
 	sort.Slice(txs, func(i, j int) bool {
 		return txs[i].Height < txs[j].Height
 	})
@@ -119,6 +133,14 @@ func (e *Exporter) FixVestingAccount(account exported.VestingAccount) error {
 	}
 
 	return nil
+}
+
+// getTransactions returns all the transactions searching for messages with the given action made
+// by the provided address at limited by the given height
+func (e *Exporter) getTransactions(action, address string, height int64) ([]*coretypes.ResultTx, error) {
+	messageQuery := "message.action = '%[1]s' AND message.sender = '%[2]s' AND tx.height <= %[3]d"
+	query := fmt.Sprintf(messageQuery, action, address, height)
+	return types.QueryTxs(e.node, query)
 }
 
 // handleMsgDelegate handles the given MsgDelegate present inside the provided
